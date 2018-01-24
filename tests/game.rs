6af87785984897dd213119ed03ad72c388b4b451
Sumner Evans <sumner.evans98@gmail.com>
2018-01-24 15:48:07 -0700

    Doing some groundwork

--- tests/game.rs
+++ tests/game.rs
@@ -1,20 +1,27 @@
 extern crate chess_pgn_parser;
 
-use chess_pgn_parser::{Game, Result};
+use chess_pgn_parser::game::{Game, GameResult};
 
 #[test]
 fn termination() {
-    assert_eq!(format!("{}", Result::WHITE_WON), "1-0");
-    assert_eq!(format!("{}", Result::BLACK_WON), "0-1");
-    assert_eq!(format!("{}", Result::DRAW), "1/2-1/2");
-    assert_eq!(format!("{}", Result::IN_PROGRESS), "*");
-    assert_eq!(format!("{}", Result::ABANDONED), "*");
-    assert_eq!(format!("{}", Result::UNKNOWN), "*");
+    assert_eq!(format!("{}", GameResult::WhiteWon), "1-0");
+    assert_eq!(format!("{}", GameResult::BlackWon), "0-1");
+    assert_eq!(format!("{}", GameResult::Draw), "1/2-1/2");
+    assert_eq!(format!("{}", GameResult::InProgress), "*");
+    assert_eq!(format!("{}", GameResult::Abandoned), "*");
+    assert_eq!(format!("{}", GameResult::Unknown), "*");
 }
 
 #[test]
 fn creating_games() {
     assert_eq!(Game::new(), Game {
+        event: "".to_string(),
+        site: "".to_string(),
+        date: None,
+        round: 0,
+        white: Vec::new(),
+        black: Vec::new(),
         result: None,
+        moves: Vec::new(),
     });
 }
